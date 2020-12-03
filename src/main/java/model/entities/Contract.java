55d32cd464a2c7a4ba60293f33a42f0fc016bdce
RafaelWhoa <rafaelwhoa@gmail.com>
2020-12-02 20:20:32 -0400

    Initial commit

---
+++ src/main/java/model/entities/Contract.java
@@ -0,0 +1,45 @@
+package model.entities;
+
+import java.util.Date;
+
+public class Contract {
+    
+    private Integer number;
+    private Date date;
+    private Double totalValue;
+
+
+    public Contract() {
+    }
+
+    public Contract(Integer number, Date date, Double totalValue) {
+        this.number = number;
+        this.date = date;
+        this.totalValue = totalValue;
+    }
+
+    public Integer getNumber() {
+        return this.number;
+    }
+
+    public void setNumber(Integer number) {
+        this.number = number;
+    }
+
+    public Date getDate() {
+        return this.date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public Double getTotalValue() {
+        return this.totalValue;
+    }
+
+    public void setTotalValue(Double totalValue) {
+        this.totalValue = totalValue;
+    }
+
+}
---
+++ src/main/java/model/services/PaypalService.java
@@ -0,0 +1,18 @@
+package model.services;
+
+public class PaypalService implements OnlinePaymentService {
+
+    @Override
+    public Double paymentFee(Double amount) {
+        
+        return null;
+    }
+
+    @Override
+    public Double interest(Double amount, Integer months) {
+        
+        return null;
+    }
+    
+
+}
